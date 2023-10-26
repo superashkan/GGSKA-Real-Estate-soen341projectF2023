0d7282d2420ce717aacde91dcc62f35fbba98553
Giancarlo <g_biasiucci@outlook.com>
2023-10-26 12:19:36 -0400

    Improved search function.

--- src/components/PropertySearchForm.js
+++ src/components/PropertySearchForm.js
@@ -1,39 +1,9 @@
 import React, { useState } from 'react'
 import { BuyList } from '../helpers/BuyList'
+import "../styles/Login.css";
 //import mysql from 'mysql'
 
 function PropertySearchForm() {
-  // var [state, setState] = useState({
-  //   minPrice: "",
-  //   maxPrice: "",
-  //   errorMessage: "Please enter at least one search criterion",
-  //   isSearchButtonDisabled: true
-  // });
-  //
-  // const performChecks = () => {
-  //   if (minPrice < 0) {
-  //     minPrice = 0;
-  //   }
-  //   if (maxPrice < 0) {
-  //     maxPrice = 0;
-  //   }
-  //   if (minPrice == 0 && maxPrice == 0) {
-  //     errorMessage = "Please choose a minimum or maximum price."
-  //     isSearchButtonDisabled = true;
-  //   }
-  //   else if (minPrice > maxPrice) {
-  //     errorMessage = "Maximum price must be greater than minumum price."
-  //     isSearchButtonDisabled = true;
-  //   }
-  //   else if (maxPrice == 0) {
-  //     errorMessage = "Maximum price must be greater than $0"
-  //     isSearchButtonDisabled = true;
-  //   }
-  //   else {
-  //     errorMessage=""
-  //     isSearchButtonDisabled = false;
-  //   }
-  // }
 
   var [minPrice, setMinPrice] = useState("");
   var [maxPrice, setMaxPrice] = useState("");
@@ -46,26 +16,40 @@ function PropertySearchForm() {
   var [isSearchButtonDisabled, setIsSearchButtonDisabled] = useState(true);
   var [results, setResults] = useState([]);
 
-  const addressCheck = async function(event) {
-    await setAddress(event.target.value);
-    console.log(address);
-    await performChecks();
+  const neatlyFormatValue = function(value) {
+    var newValueStr = "";
+    var forwardPositionCounter = 0;
+    for (var i = value.toString().length - 1;i >= 0;i--) {
+      if ((forwardPositionCounter % 3 == 0 && forwardPositionCounter > 0)) {
+        newValueStr = "," + newValueStr;
+      }
+      newValueStr = value.toString()[i] + newValueStr;
+      forwardPositionCounter++;
+    }
+    return newValueStr;
   }
 
-  const bedroomsCheck = async function(event) {
-    if (event.target.value < 0) {
-      event.target.value = 0;
-    }
-    await setBedrooms(parseInt(event.target.value));
-    await performChecks();
+  const addressCheck = function(event) {
+    setAddress(event.target.value, () => {
+      console.log(address);
+    });
+    performChecks();
   }
 
-  const bathroomsCheck = async function(event) {
-    if (event.target.value < 0) {
+  const bedroomsCheck = function(event) {
+    if (parseInt(event.target.value) < 0) {
       event.target.value = 0;
     }
-    await setBathrooms(parseInt(event.target.value));
-    await performChecks();
+    setBedrooms(parseInt(event.target.value));
+    performChecks();
+  }
+
+  const bathroomsCheck = function(event) {
+    if (parseInt(event.target.value) < 0) {
+      event.target.value = 0;
+    }
+    setBathrooms(parseInt(event.target.value));
+    performChecks();
   }
 
   const isNullOrEmpty = function(stringInput) {
@@ -76,8 +60,13 @@ function PropertySearchForm() {
     }
   }
 
-  const performSearch = async function() {
+  const performSearch = function() {
     var resultsList = BuyList;
+    const res = performChecks();
+    if (res == false) {
+      results = [];
+      return;
+    }
     if (!isNullOrEmpty(address)) {
       resultsList = resultsList.filter(function(property) {
         return property.address.toLowerCase().includes(address.toLowerCase());
@@ -95,12 +84,12 @@ function PropertySearchForm() {
     }
     if (!isNullOrEmpty(maxPrice)) {
       resultsList = resultsList.filter(function(property) {
-        return property.price <= maxPrice;
+        return parseFloat(property.price) <= maxPrice;
       });
     }
     if (!isNullOrEmpty(minPrice)) {
       resultsList = resultsList.filter(function(property) {
-        return property.price >= minPrice;
+        return parseFloat(property.price) >= minPrice;
       });
     }
     if (!isNullOrEmpty(bedrooms)) {
@@ -113,10 +102,10 @@ function PropertySearchForm() {
         return property.bathrooms == bathrooms;
       });
     }
-    await setResults(resultsList);
+    setResults(resultsList);
   }
 
-  const performChecks = async function() {
+  const performChecks = function() {
     if (address == "") {
       if (minPrice == 0 || minPrice == "") {
         if (maxPrice == 0 || maxPrice == "") {
@@ -124,9 +113,9 @@ function PropertySearchForm() {
             if (bathrooms == "") {
               if (minSize == 0 || minSize == "") {
                 if (maxSize == 0 || maxSize == "") {
-                  await setErrorMessage("Please enter at least one search criterion.");
-                  await setIsSearchButtonDisabled(true)
-                  return;
+                  setErrorMessage("Please enter at least one search criterion.");
+                  setIsSearchButtonDisabled(true)
+                  return false;
                 }
               }
             }
@@ -134,109 +123,161 @@ function PropertySearchForm() {
         }
       }
     }
-    else if (bedrooms != "" && (bedrooms < 1 || bedrooms > 5)) {
-      await setErrorMessage("If choosing a number of bedrooms, please choose a number between 1 and 5.");
-      await setIsSearchButtonDisabled(true)
+    if (minPrice < 0.00) {
+      setErrorMessage("Minimum price must not be negative.");
+      setIsSearchButtonDisabled(true);
+      return false;
     }
-    else if (bathrooms != "" && (bathrooms < 1 || bathrooms > 5)) {
-      await setErrorMessage("If choosing a number of bathrooms, please choose a number between 1 and 5.");
-      await setIsSearchButtonDisabled(true)
+    if (maxPrice < 0.00) {
+      setErrorMessage("Maximum price must not be negative.");
+      setIsSearchButtonDisabled(true);
+      return false;
     }
-    else if ((minPrice == 0 && maxPrice == 0) && (minPrice.toString() != '' && maxPrice.toString() != '')) {
-      await setErrorMessage("Please choose a minimum or maximum price.");
-      await setIsSearchButtonDisabled(true)
+    if (minSize < 0.00) {
+      setErrorMessage("Minimum property size must not be negative.");
+      setIsSearchButtonDisabled(true);
+      return false;
     }
-    else if ((minSize == 0 && maxSize == 0) && (minSize.toString() != '' && maxSize.toString() != '')) {
-      await setErrorMessage("Please choose a minimum or maximum lot size.");
-      await setIsSearchButtonDisabled(true)
+    if (maxSize < 0.00) {
+      setErrorMessage("Maximum size must not be negative.");
+      setIsSearchButtonDisabled(true);
+      return false;
     }
-    else if (minPrice > maxPrice) {
-      await setErrorMessage("Maximum price must be greater than minimum price.");
-      await setIsSearchButtonDisabled(true)
+    if (bedrooms != "" && (bedrooms < 1 || bedrooms > 5)) {
+      setErrorMessage("If choosing a number of bedrooms, please choose a number between 1 and 5.");
+      setIsSearchButtonDisabled(true);
+      return false;
     }
-    else if (minSize > maxSize) {
-      await setErrorMessage("Maximum lot size must be greater than minimum lot size.");
-      await setIsSearchButtonDisabled(true)
+    if (bathrooms != "" && (bathrooms < 1 || bathrooms > 5)) {
+      setErrorMessage("If choosing a number of bathrooms, please choose a number between 1 and 5.");
+      setIsSearchButtonDisabled(true);
+      return false;
     }
-    else if (maxPrice == 0 && maxPrice.toString() != '') {
-      await setErrorMessage("Maximum price must be greater than $0");
-      await setIsSearchButtonDisabled(true);
+    if ((minPrice == 0 && maxPrice == 0) && (minPrice.toString() != '' && maxPrice.toString() != '')) {
+      setErrorMessage("Please choose a minimum or maximum price.");
+      setIsSearchButtonDisabled(true);
+      return false;
     }
-    else {
-      await setErrorMessage("");
-      await setIsSearchButtonDisabled(false);
-      await performSearch();
+    if ((minSize == 0 && maxSize == 0) && (minSize.toString() != '' && maxSize.toString() != '')) {
+      setErrorMessage("Please choose a minimum or maximum lot size.");
+      setIsSearchButtonDisabled(true);
+      return false;
     }
+    if (minPrice > maxPrice && maxPrice != '') {
+      setErrorMessage("Maximum price must be greater than minimum price.");
+      setIsSearchButtonDisabled(true);
+      return false;
+    }
+    if (minSize > maxSize  && maxSize != '') {
+      setErrorMessage("Maximum lot size must be greater than minimum lot size.");
+      setIsSearchButtonDisabled(true);
+      return false;
+    }
+    if (maxPrice == 0 && maxPrice.toString() != '') {
+      setErrorMessage("Maximum price must be greater than $0");
+      setIsSearchButtonDisabled(true);
+      return false;
+    }
+    setErrorMessage("");
+    setIsSearchButtonDisabled(false);
+    return true;
   }
 
-  const checkIfValueNegative = async function(event) {
+  const checkIfValueNegative = function(event) {
     if (event.target.value < 0) {
       event.target.value = 0;
     }
     if (event.target.id == "min_price") {
-      await setMinPrice(event.target.value);
+      setMinPrice(parseFloat(event.target.value));
     }
     if (event.target.id == "max_price") {
-      await setMaxPrice(event.target.value);
+      setMaxPrice(parseFloat(event.target.value));
     }
     if (event.target.id == "min_size") {
-      await setMinSize(event.target.value);
+      setMinSize(parseFloat(event.target.value));
     }
     if (event.target.id == "max_size") {
-      await setMaxSize(event.target.value);
+      setMaxSize(parseFloat(event.target.value));
     }
-    await performChecks();
+    performChecks();
   }
 
-  return (
-    <div>
-      <form id="search-form">
-        <label htmlFor="address">Address</label>
-        <input name="address" id="address" placeholder="Address" type="text" onInput={(event) => addressCheck(event)}/>
-        <label htmlFor="min_price">Minimum Price</label>
-        <input name="min_price" id="min_price" placeholder="Minimum price" type="number" onInput={(event) => checkIfValueNegative(event)} />
-        <label htmlFor="max_price">Maximum Price</label>
-        <input name="max_price" id="max_price" placeholder="Maximum price" type="number" onInput={(event) => checkIfValueNegative(event)}/>
-        <label htmlFor="numBedrooms"># of Bedrooms</label>
-        <input id="numBedrooms" name="numBedrooms" placeholder="# of bedrooms (1 to 5)" type="number" onInput={(event) => bedroomsCheck(event)} />
-        <label htmlFor="numBathrooms"># of Bathrooms</label>
-        <input id="numBathrooms" name="numBathrooms" placeholder="# of bathrooms (1 to 5)" type="number" onInput={(event) => bathroomsCheck(event)} />
-        <label htmlFor="min_price">Minimum Lot Size</label>
-        <input name="min_size" id="min_size" placeholder="Minimum Lot Size (sqft.)" type="number" onInput={(event) => checkIfValueNegative(event)} />
-        <label htmlFor="max_price">Maximum Lot Size</label>
-        <input name="max_size" id="max_size" placeholder="Maximum Lot Size (sqft.)" type="number" onInput={(event) => checkIfValueNegative(event)}/>
-        <div id="errorMessage">{errorMessage}</div>
-        <button type="submit" disabled={isSearchButtonDisabled}> Search </button>
-      </form>
-
-      <table>
-        <thead>
-          <tr>
-            <th>Address</th>
-            <th>Price</th>
-            <th>Type</th>
-            <th>Lot Size</th>
-            <th># of Bedrooms</th>
-            <th># of Bathrooms</th>
-          </tr>
-        </thead>
-        <tbody>
-          {results.map((result) => {
-            return (
+  const constructHTML = function() {
+    if (results.length == 0) {
+      return (
+        <div>
+        <form id="search-form">
+          <label htmlFor="address">Address</label>
+          <input name="address" id="address" placeholder="Address" type="text" onInput={(event) => setAddress(event.target.value)}/>
+          <label htmlFor="min_price">Minimum Price</label>
+          <input name="min_price" id="min_price" placeholder="Minimum price" type="number" onInput={(event) => setMinPrice(event.target.value)} />
+          <label htmlFor="max_price">Maximum Price</label>
+          <input name="max_price" id="max_price" placeholder="Maximum price" type="number" onInput={(event) => setMaxPrice(event.target.value)}/>
+          <label htmlFor="numBedrooms"># of Bedrooms</label>
+          <input id="numBedrooms" name="numBedrooms" placeholder="# of bedrooms (1 to 5)" type="number" onInput={(event) => setBedrooms(event.target.value)} />
+          <label htmlFor="numBathrooms"># of Bathrooms</label>
+          <input id="numBathrooms" name="numBathrooms" placeholder="# of bathrooms (1 to 5)" type="number" onInput={(event) => setBathrooms(event.target.value)} />
+          <label htmlFor="min_price">Minimum Lot Size</label>
+          <input name="min_size" id="min_size" placeholder="Minimum Lot Size (sqft.)" type="number" onInput={(event) => setMinSize(event.target.value)} />
+          <label htmlFor="max_price">Maximum Lot Size</label>
+          <input name="max_size" id="max_size" placeholder="Maximum Lot Size (sqft.)" type="number" onChange={(event) => setMaxSize(event.target.value)}/>
+          <div id="errorMessage">{errorMessage}</div>
+        </form>
+        <button className="form button" onClick={event => performSearch()}> Test Search </button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <form id="search-form">
+            <label htmlFor="address">Address</label>
+            <input name="address" id="address" placeholder="Address" type="text" onInput={(event) => setAddress(event.target.value)}/>
+            <label htmlFor="min_price">Minimum Price</label>
+            <input name="min_price" id="min_price" placeholder="Minimum price" type="number" onInput={(event) => setMinPrice(event.target.value)} />
+            <label htmlFor="max_price">Maximum Price</label>
+            <input name="max_price" id="max_price" placeholder="Maximum price" type="number" onInput={(event) => setMaxPrice(event.target.value)}/>
+            <label htmlFor="numBedrooms"># of Bedrooms</label>
+            <input id="numBedrooms" name="numBedrooms" placeholder="# of bedrooms (1 to 5)" type="number" onInput={(event) => setBedrooms(event.target.value)} />
+            <label htmlFor="numBathrooms"># of Bathrooms</label>
+            <input id="numBathrooms" name="numBathrooms" placeholder="# of bathrooms (1 to 5)" type="number" onInput={(event) => setBathrooms(event.target.value)} />
+            <label htmlFor="min_price">Minimum Lot Size</label>
+            <input name="min_size" id="min_size" placeholder="Minimum Lot Size (sqft.)" type="number" onInput={(event) => setMinSize(event.target.value)} />
+            <label htmlFor="max_price">Maximum Lot Size</label>
+            <input name="max_size" id="max_size" placeholder="Maximum Lot Size (sqft.)" type="number" onChange={(event) => setMaxSize(event.target.value)}/>
+            <div id="errorMessage">{errorMessage}</div>
+          </form>
+          <table>
+            <thead>
               <tr>
-                <td>{result.address}</td>
-                <td>{result.price}</td>
-                <td>{result.type}</td>
-                <td>{result.lotSize}</td>
-                <td>{result.bedrooms}</td>
-                <td>{result.bathrooms}</td>
+                <th>Address</th>
+                <th>Price</th>
+                <th>Type</th>
+                <th>Lot Size</th>
+                <th># of Bedrooms</th>
+                <th># of Bathrooms</th>
               </tr>
-            )})
-          }
-        </tbody>
-      </table>
-    </div>
-  )
-}
+            </thead>
+            <tbody>
+              {results.map((result) => {
+                return (
+                  <tr>
+                    <td>{result.address}</td>
+                    <td>${neatlyFormatValue(result.price)}</td>
+                    <td>{result.type}</td>
+                    <td>{neatlyFormatValue(result.lotSize)} sqft.</td>
+                    <td>{result.bedrooms}</td>
+                    <td>{result.bathrooms}</td>
+                  </tr>
+                )})
+              }
+            </tbody>
+          </table>
+          <button className="form button" onClick={event => performSearch()}> Test Search </button>
+        </div>
+      );
+    }
+  }
 
+  return constructHTML();
+}
 export default PropertySearchForm
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,59 +1,8 @@
 import React from 'react'
-import { useState } from 'react'
 import PropertySearchForm from '../components/PropertySearchForm'
-//import { propertyList } from '../propertyList.js'
-
-function isNullOrEmpty(stringInput) {
-  if (stringInput === null || stringInput.toString().trim() === "") {
-    return true;
-  } else {
-    return false;
-  }
-}
+import "../styles/Login.css";
 
 function Search() {
-  // var [state, setState] = useState({
-  //   minPriceTracker: 0.00,
-  //   maxPriceTracker: 0.00,
-  //   errorMessage: "Please enter at least one search criterion",
-  //   isSearchButtonDisabled: true
-  // });
-  // return (
-  //   <div>
-  //     <h1> Search </h1>
-  //
-  //     <form id="search-form" method="GET">
-  //       <label htmlFor="address">Address</label>
-  //       <input name="address" id="address" placeholder="Address" type="text" />
-  //       <label htmlFor="min_price">Minimum Price</label>
-  //       <input name="min_price" id="min_price" placeholder="Minimum price" type="number" onBlur={performChecks(this.state.minPriceTracker, this.state.maxPriceTracker, this.state.errorMessage, this.state.isSearchButtonDisabled)} />
-  //       <label htmlFor="max_price">Maximum Price</label>
-  //       <input name="max_price" id="max_price" placeholder="Maximum price" type="number" onBlur={performChecks(this.state.minPriceTracker, this.state.maxPriceTracker, this.state.errorMessage, this.state.isSearchButtonDisabled)} />
-  //       <label htmlFor="numBedrooms"># of Bedrooms</label>
-  //       <select id="numBedrooms" name="numBedrooms" defaultValue="">
-  //           <option value="" disabled>Select Beds</option>
-  //           <option value="noselection">No Selection</option>
-  //           <option value="1">1 Bed</option>
-  //           <option value="2">2 Beds</option>
-  //           <option value="3">3 Beds</option>
-  //           <option value="4">4 Beds</option>
-  //           <option value="5">5 Beds</option>
-  //       </select>
-  //       <label htmlFor="numBathrooms"># of Bathrooms</label>
-  //       <select id="numBathrooms" name="numBathrooms" defaultValue="">
-  //           <option value="" disabled>Select Beds</option>
-  //           <option value="noselection">No Selection</option>
-  //           <option value="1">1 Bed</option>
-  //           <option value="2">2 Beds</option>
-  //           <option value="3">3 Beds</option>
-  //           <option value="4">4 Beds</option>
-  //           <option value="5">5 Beds</option>
-  //       </select>
-  //       <div id="errorMessage">{this.state.errorMessage}</div>
-  //       <button type="submit" disabled={this.state.isSearchButtonDisabled}> Search </button>
-  //     </form>
-  //   </div>
-  // )
   return (
     <div>
       <PropertySearchForm />
