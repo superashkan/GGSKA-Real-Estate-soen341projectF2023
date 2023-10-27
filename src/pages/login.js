dc5d0ba80fa232a4423acd966e92e1eb55862459
Ashkan Forghani <130598631+superashkan@users.noreply.github.com>
2023-10-27 18:02:57 -0400

    Update login.js
    Now with login functionality

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,30 +1,44 @@
-import React from 'react'
-import '../styles/MultiPageCSS.css'
-import { Link } from 'react-router-dom'
+import {Link, Navigate} from "react-router-dom";
+import {useContext, useState} from "react";
+import axios from "axios";
 
 
-function login() {
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleLoginSubmit(ev) {
+    ev.preventDefault();
+    try{
+      await axios.post('/login', {email,password});
+      alert('Login successful')
+    }
+    catch (e){
+      alert('Login failed')
+    }
+  }
   return (
         <div>
             <h1 className='title'> Login </h1>
+    
+            <form id="contact-form" onSubmit={handleLoginSubmit}>
 
-            <form id="contact-form" method="POST">
-              <label htmlFor="name">Full Name</label>
-              <input name="name" placeholder="Enter full name..." type="text" />
               <label htmlFor="email">Email</label>
-              <input name="email" placeholder="Enter email..." type="email" />
+              <input name="email" placeholder="Enter email..." type="email" onChange={ev => setEmail(ev.target.value)}/>
+
               <label htmlFor="password">Password</label>
-              <input name="password" placeholder="Enter password..." type="email" />
+              <input name="password" placeholder="Enter password..." type="password" onChange={ev => setPassword(ev.target.value)}/>
 
               <div className='notRegistered'>
-              <Link to='/register' className='notYetRegistered'> Not yet registered? Do it now!</Link>
-              </div>
+              <Link to='/register'> Not yet registered? Do it now!</Link>
+              </div>  
 
               <button type="submit"> Login </button>
             </form>
         </div>
       );
     }
+    
 
-
-export default login
+export default Login
