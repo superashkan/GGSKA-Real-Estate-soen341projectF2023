9593f8328cd7f07514e6b7f4ec97c66e6dc58c9a
Ashkan Forghani <130598631+superashkan@users.noreply.github.com>
2023-10-27 18:03:21 -0400

    Update register.js

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,30 +1,56 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import '../styles/MultiPageCSS.css'
+import { useState } from 'react';
+import axios from 'axios';
+import '../styles/Login.css'
 
-function register() {
+function Register() {
+  const [name,setName] = useState("");
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState('');
+  
+  async function registerUser(ev) {
+    ev.preventDefault();
+    try {
+      await axios.post('/register', {
+        name,
+        email,
+        password,
+      });
+      alert('Registration successful. Now you can log in');
+    } 
+    
+    catch (e) {
+      alert('Registration failed. Please try again later');
+    }
+
+  }
+
+
   return (
     <div>
-      <div className='title'>
+      <div className='registerTitle'>
         <h1> Register </h1>
       </div>
 
-      <form id="contact-form" method="POST">
+      <form id="contact-form" onSubmit={registerUser}>
         <label htmlFor="name">Full Name</label>
-        <input name="name" placeholder="Enter full name..." type="text" />
+        <input name="name" placeholder="Enter full name..." type="text" onChange={(e) => setName(e.target.value)}/>
+
         <label htmlFor="email">Email</label>
-        <input name="email" placeholder="Enter email..." type="email" />
+        <input name="email" placeholder="Enter email..." type="email" onChange={(e) => setEmail(e.target.value)}/>
+
         <label htmlFor="password">Password</label>
-        <input name="password" placeholder="Enter password..." type="text" />
+        <input name="password" placeholder="Enter password..." type="password" onChange={(e) => setPassword(e.target.value)}/>
 
         <div className='notRegistered'>
-        <Link to='/login' className="alreadyRegistered"> Already registered? Login</Link>
-        </div>
+        <Link to='/login'> Already registered? Login</Link>
+        </div>  
 
-        <button type="submit"> Login </button>
+        <button type="submit"> Register </button>
       </form>
     </div>
   )
 }
 
-export default register
+export default Register
