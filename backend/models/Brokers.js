24e6102caa63bf930a2e9c9004d6a692d749bb92
Ashkan Forghani <130598631+superashkan@users.noreply.github.com>
2023-11-08 15:39:15 -0500

    Update Brokers.js

--- backend/models/Brokers.js
+++ backend/models/Brokers.js
@@ -5,6 +5,9 @@ const BrokerSchema = new mongoose.Schema({
     name: String,
     email: {type: String, unique: true},
     password: String,
+    age: Number,
+    phone_number: String,
+    license_number: Number
 })
 
 const BrokerModel = new mongoose.model("brokers", BrokerSchema)
